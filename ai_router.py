e4e2faf6c35de6096106f4686c1ebd3324b9872b
JiwonHwang84 <ghkdwldnjs112@gmail.com>
2024-10-28 16:25:42 +0900

    feat: get_stocks 인자 수정

--- ai_router.py
+++ ai_router.py
@@ -51,7 +51,7 @@ async def post_newspaper(
 async def get_stocks(news_id: int):
     try:
         # 기사 ID로 주식 정보를 추출하고 데이터베이스에 저장
-        stocks = ai_service.save_stock_info_to_db(newspaper.id)  # newspaper 객체의 id 필드를 사용
+        stocks = ai_service.save_stock_info_to_db(news_id)  # newspaper 객체의 id 필드를 사용
         return stocks
     except HTTPException as e:
         raise e
